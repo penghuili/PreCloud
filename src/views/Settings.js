3b30e996082313ac07a4cf9852c30019feb506ba
Peng <1a84@proton.me>
2022-10-22 23:30:45 +0200

    fix: new xiangcai

--- src/views/Settings.js
+++ src/views/Settings.js
@@ -133,7 +133,7 @@ function Settings({ navigation, route: { name: routeName } }) {
           <VStack space={1}>
             <Text>This is my cat, Xiangcai</Text>
 
-            <Image source={xiangcai} alt="My cat" size="xl" />
+            <Image source={xiangcai} alt="My cat" size="2xl" resizeMode="cover" />
 
             <Text fontSize="xs" mt="1" color="gray.500">
               Want to show your pets?{' '}
