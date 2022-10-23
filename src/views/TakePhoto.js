99c890f4719f3211da4d393d6e29534bb9ca0f09
Peng <1a84@proton.me>
2022-10-23 19:34:12 +0200

    fix: add hint for taking photo

--- src/views/TakePhoto.js
+++ src/views/TakePhoto.js
@@ -1,5 +1,24 @@
+import { Text } from 'native-base';
+import React from 'react';
+
+import AppBar from '../components/AppBar';
+import ContentWrapper from '../components/ContentWrapper';
+import Icon from '../components/Icon';
+import ScreenWrapper from '../components/ScreenWrapper';
+import useColors from '../hooks/useColors';
+
 function TakePhoto() {
-  return null;
+  const colors = useColors();
+  return (
+    <ScreenWrapper>
+      <AppBar title="Take photo" />
+      <ContentWrapper>
+        <Text alignItems="center">
+          Press the <Icon name="camera-outline" color={colors.text} size={20} /> below again.
+        </Text>
+      </ContentWrapper>
+    </ScreenWrapper>
+  );
 }
 
 export default TakePhoto;
