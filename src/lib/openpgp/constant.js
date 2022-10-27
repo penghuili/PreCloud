f38dbaa84e9b8c7229d4949435b9239745ef54c6
Peng <1a84@proton.me>
2022-10-27 20:08:35 +0200

    fix: downgrade to 1g

--- src/lib/openpgp/constant.js
+++ src/lib/openpgp/constant.js
@@ -4,6 +4,6 @@ export const openpgpStatus = {
   error: 'openpgpStatus/error',
 };
 
-export const ENCRYPTION_LIMIT_IN_GIGABYTES = 1.25;
+export const ENCRYPTION_LIMIT_IN_GIGABYTES = 1;
 export const ENCRYPTION_LIMIT_IN_BYTES = ENCRYPTION_LIMIT_IN_GIGABYTES * 1024 * 1024 * 1024;
 export const LARGE_FILE_SIZE_IN_BYTES = 100 * 1024 * 1024;
